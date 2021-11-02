2ecc57290318c37bc5e5475889be5365d9433afb
Steve McGuire <s.mcguire@hud.ac.uk>
2021-11-02 11:09:33 +0000

    Number guess

--- week2/operators.py
+++ week2/operators.py
@@ -4,3 +4,26 @@ __contact__ = "s.mcguire@hud.ac.uk"
 
 num1 = int('3.14')
 print (num1)
+
+name = "Steve"
+print(type(name))
+print(name)
+age = 43
+print(type(age))
+print(age)
+height = 5.11
+print(type(height))
+print(height)
+
+print(name == "Steve") # is equal to / comparison
+print(name == "Steve" and age == 19)
+
+has_license = False
+
+print(has_license and age >= 17)
+
+if has_license and age >= 17:
+    print("You can hire a car")
+else:
+    print("You cannot hire a car")
+
